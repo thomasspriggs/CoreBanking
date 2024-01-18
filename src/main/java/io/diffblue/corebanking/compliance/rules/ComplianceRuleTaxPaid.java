a5d25b52d1fcf560daa769a4fbe0c72adcef3765
Thomas Spriggs <35029362+thomasspriggs@users.noreply.github.com>
2024-01-18 10:41:14 +0000

    Switch to Java style naming convention

--- src/main/java/io/diffblue/corebanking/compliance/rules/ComplianceRuleTaxPaid.java
+++ src/main/java/io/diffblue/corebanking/compliance/rules/ComplianceRuleTaxPaid.java
@@ -24,16 +24,16 @@ public class ComplianceRuleTaxPaid extends ComplianceRule {
     {
         if(incomeEarnedThisYear <= LIMIT_PERSONAL_ALLOWANCE)
             return new TaxDue(0, "No tax due as income is below personal allowance limit");
-        final int taxable_at_basic_rate = min(incomeEarnedThisYear, LIMIT_BASIC_RATE) - LIMIT_PERSONAL_ALLOWANCE;
-        final int basic_rate_tax = (taxable_at_basic_rate * BASIC_RATE_PERCENT) / 100;
+        final int taxableAtBasicRate = min(incomeEarnedThisYear, LIMIT_BASIC_RATE) - LIMIT_PERSONAL_ALLOWANCE;
+        final int basicRateTax = (taxableAtBasicRate * BASIC_RATE_PERCENT) / 100;
         if(incomeEarnedThisYear <= LIMIT_BASIC_RATE)
-            return new TaxDue(basic_rate_tax, "Basic rate tax is due");
-        final int taxable_at_higher_rate = min(incomeEarnedThisYear, LIMIT_HIGHER_RATE) - LIMIT_BASIC_RATE;
-        final int higher_rate_tax = (taxable_at_higher_rate * HIGHER_RATE_PERCENT) / 100;
+            return new TaxDue(basicRateTax, "Basic rate tax is due");
+        final int taxableAtHigherRate = min(incomeEarnedThisYear, LIMIT_HIGHER_RATE) - LIMIT_BASIC_RATE;
+        final int higher_rate_tax = (taxableAtHigherRate * HIGHER_RATE_PERCENT) / 100;
         if(incomeEarnedThisYear <= LIMIT_HIGHER_RATE)
-            return new TaxDue(basic_rate_tax + higher_rate_tax, "Higher and basic rate tax is due");
-        final int taxable_at_additional_rate = incomeEarnedThisYear - ADDITIONAL_RATE;
-        final int additional_rate_tax = (taxable_at_additional_rate * ADDITIONAL_RATE) / 100;
-        return new TaxDue(additional_rate_tax + basic_rate_tax + higher_rate_tax, "Additional, higher and basic rate tax is due.");
+            return new TaxDue(basicRateTax + higher_rate_tax, "Higher and basic rate tax is due");
+        final int taxableAtAdditionalRate = incomeEarnedThisYear - ADDITIONAL_RATE;
+        final int additionalRateTax = (taxableAtAdditionalRate * ADDITIONAL_RATE) / 100;
+        return new TaxDue(additionalRateTax + basicRateTax + higher_rate_tax, "Additional, higher and basic rate tax is due.");
     }
 }
