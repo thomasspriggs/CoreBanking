7b787e52ea429d3d30f995e41c03da6604cbe730
Thomas Spriggs <35029362+thomasspriggs@users.noreply.github.com>
2024-01-18 10:39:12 +0000

    Switch to <=

--- src/main/java/io/diffblue/corebanking/compliance/rules/ComplianceRuleTaxPaid.java
+++ src/main/java/io/diffblue/corebanking/compliance/rules/ComplianceRuleTaxPaid.java
@@ -22,15 +22,15 @@ public class ComplianceRuleTaxPaid extends ComplianceRule {
 
     public TaxDue calculateTax(int incomeEarnedThisYear)
     {
-        if(incomeEarnedThisYear < LIMIT_PERSONAL_ALLOWANCE)
+        if(incomeEarnedThisYear <= LIMIT_PERSONAL_ALLOWANCE)
             return new TaxDue(0, "No tax due as income is below personal allowance limit");
         final int taxable_at_basic_rate = min(incomeEarnedThisYear, LIMIT_BASIC_RATE) - LIMIT_PERSONAL_ALLOWANCE;
         final int basic_rate_tax = (taxable_at_basic_rate * BASIC_RATE_PERCENT) / 100;
-        if(incomeEarnedThisYear < LIMIT_BASIC_RATE)
+        if(incomeEarnedThisYear <= LIMIT_BASIC_RATE)
             return new TaxDue(basic_rate_tax, "Basic rate tax is due");
         final int taxable_at_higher_rate = min(incomeEarnedThisYear, LIMIT_HIGHER_RATE) - LIMIT_BASIC_RATE;
         final int higher_rate_tax = (taxable_at_higher_rate * HIGHER_RATE_PERCENT) / 100;
-        if(incomeEarnedThisYear < LIMIT_HIGHER_RATE)
+        if(incomeEarnedThisYear <= LIMIT_HIGHER_RATE)
             return new TaxDue(basic_rate_tax + higher_rate_tax, "Higher and basic rate tax is due");
         final int taxable_at_additional_rate = incomeEarnedThisYear - ADDITIONAL_RATE;
         final int additional_rate_tax = (taxable_at_additional_rate * ADDITIONAL_RATE) / 100;
